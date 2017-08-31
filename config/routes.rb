3f55af50ebc274c6f0ada05c01b0b1a37f1a861f
rekha014 <rekhdhakad014@gmail.com>
2017-08-31 13:00:10 +0530

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   get 'home/index'
   root "home#index"
 
+  get 'about', to: 'home#about', as: 'about'
+  get '/products', to: 'product#index', as: 'products'
+  get 'contact', to: 'contact#new', as: 'contact'
+  post 'contact', to: 'contact#create_mail'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
